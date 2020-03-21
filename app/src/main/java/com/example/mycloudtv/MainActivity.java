fa11496f16ab7d792b8bb10c0cbfdb23fe00e5b3
Administrator <631442030@qq.com>
2020-03-21 15:47:51 +0800

    左边选中效果

--- app/src/main/java/com/example/mycloudtv/MainActivity.java
+++ app/src/main/java/com/example/mycloudtv/MainActivity.java
@@ -7,38 +7,45 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.example.mycloudtv.dialog.WebDialog;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentTransaction;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends FragmentActivity {
-    @BindView(R.id.tvExit)
-    TextView tvExit;
     @BindView(R.id.topLayout)
     LinearLayout topLayout;
-    @BindView(R.id.tvPai)
-    TextView tvPai;
-    @BindView(R.id.tvChe)
-    TextView tvChe;
-    @BindView(R.id.tvRenJi)
-    TextView tvRenJi;
     @BindView(R.id.leftLayout)
     LinearLayout leftLayout;
     @BindView(R.id.fragmentLayout)
     FrameLayout fragmentLayout;
+    @BindView(R.id.viewPai)
+    View viewPai;
+    @BindView(R.id.llPai)
+    LinearLayout llPai;
+    @BindView(R.id.viewChe)
+    View viewChe;
+    @BindView(R.id.llChe)
+    LinearLayout llChe;
+    @BindView(R.id.viewRenJi)
+    View viewRenJi;
+    @BindView(R.id.llRenJi)
+    LinearLayout llRenJi;
+    @BindView(R.id.viewExit)
+    View viewExit;
+    @BindView(R.id.llExit)
+    LinearLayout llExit;
 
 
     private Fragment currentFragment = new Fragment();
 
     WebDialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,27 +73,32 @@ public class MainActivity extends FragmentActivity {
         return transaction;
     }
 
-    @OnClick({R.id.tvPai, R.id.tvChe, R.id.tvRenJi, R.id.tvExit})
+    @OnClick({R.id.llPai, R.id.llChe, R.id.llRenJi, R.id.llExit})
     public void onViewClicked(View view) {
         switch (view.getId()) {
-            case R.id.tvPai:
+            case R.id.llPai:
                 switchFragment(new FirstFragment()).commit();
+                selectView(view.getId());
                 showWebDialog();
                 break;
-            case R.id.tvChe:
+            case R.id.llChe:
                 switchFragment(new WorkShopFragment()).commit();
+                selectView(view.getId());
                 break;
-            case R.id.tvRenJi:
+            case R.id.llRenJi:
                 switchFragment(new ThirdFragment()).commit();
+                selectView(view.getId());
                 break;
-            case R.id.tvExit:
+            case R.id.llExit:
                 switchFragment(new ExitFragment()).commit();
+                selectView(view.getId());
                 break;
         }
     }
 
+
     private void showWebDialog() {
-        dialog =new WebDialog(MainActivity.this);
+        dialog = new WebDialog(MainActivity.this);
         dialog.show();
         //获取dialog布局
         WindowManager.LayoutParams params = dialog.getWindow().getAttributes();
@@ -100,4 +112,31 @@ public class MainActivity extends FragmentActivity {
         //使设置生效
         dialog.getWindow().setAttributes(params);
     }
+
+    /**
+     * 选中样式
+     *
+     * @param viewId 选中view的ID
+     */
+    private void selectView(int viewId) {
+        viewPai.setVisibility(View.INVISIBLE);
+        viewChe.setVisibility(View.INVISIBLE);
+        viewRenJi.setVisibility(View.INVISIBLE);
+        viewExit.setVisibility(View.INVISIBLE);
+        switch (viewId) {
+            case R.id.llPai:
+                viewPai.setVisibility(View.VISIBLE);
+                break;
+            case R.id.llChe:
+                viewChe.setVisibility(View.VISIBLE);
+                break;
+            case R.id.llRenJi:
+                viewRenJi.setVisibility(View.VISIBLE);
+                break;
+            case R.id.llExit:
+                viewExit.setVisibility(View.VISIBLE);
+                break;
+        }
+    }
+
 }
